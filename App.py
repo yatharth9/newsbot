2deb460da11c80b8a8e4454032260c3180e5abdd
Yatharth <shah9yatharth@gmail.com>
2021-03-19 19:07:00 +0530

    Functional version

--- App.py
+++ App.py
@@ -10,14 +10,14 @@ newsbot.single("Term"): Returns the news feed for the term
 newsbot.sources() : Returns the sources currently being resolved```
 """
 
-links = {"Wired"          : "https://www.wired.com/feed/category/gear/latest/rss",
-         "Verge"          : "https://www.theverge.com/rss/index.xml",
-         "Techmeme"       : "https://www.techmeme.com/feed.xml",
-         "Techcrunch"     : "https://techcrunch.com/tag/rss/",
-         "Pc world"       : "https://www.pcworld.com/index.rss",
-         "Engadget"       : "https://www.engadget.com/rss.xml",
-         "Digital Trends" : "https://www.digitaltrends.com/computing/feed/",
-         "Cnet"           : "https://www.cnet.com/rss/news/"}
+links = {"wired"          : "https://www.wired.com/feed/category/gear/latest/rss",
+         "verge"          : "https://www.theverge.com/rss/index.xml",
+         "techmeme"       : "https://www.techmeme.com/feed.xml",
+         "techcrunch"     : "https://techcrunch.com/tag/rss/",
+         "pc world"       : "https://www.pcworld.com/index.rss",
+         "engadget"       : "https://www.engadget.com/rss.xml",
+         "digital Trends" : "https://www.digitaltrends.com/computing/feed/",
+         "cnet"           : "https://www.cnet.com/rss/news/"}
 
 def list_sources():
     sources =[]
@@ -25,20 +25,26 @@ def list_sources():
         sources.append(x)
     return sources
 
-link = links["Verge"]
+key = links.keys()
+
+#link = links["Verge"]
 #print(link)
 def parse_single(url):
+    renders = []
     try:
         news = feedparser.parse(url)
         for i in range(len(news.entries)):
-            print(f"Title: {news.entries[i].title}\n Link: {news.entries[i].link}")
+            #print(f"Title: {news.entries[i].title}\n Link: {news.entries[i].link}")
+            renders.append(news.entries[i].title)
+            renders.append(news.entries[i].link)
+        return renders
     except:
         print(f"Error resolving feed: {url}")
 
 #parse_single(link)
 
 def search_term(message):
-    try:
+    #try:
         match = re.fullmatch('newsbot.single\((["|\'])([^\)]+)(["|\'])\)', message)
         if match.group(1) != match.group(3):
             return False
@@ -46,12 +52,13 @@ def search_term(message):
             if match.group(2)[check.start()-1] != '\\':
                 return False
         return match.group(2)
-    except:
+    #except:
         return False
 
 istr = 'newsbot.single("Hello")'
 
 search_term(message=istr)
+
 intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
@@ -76,7 +83,26 @@ async def on_message(message):
     elif "newsbot.sources()" == message.content.lower():
         await message.channel.send(f"The sources are: ")
         source = list_sources()
-        #for x in source:
+        print(len(source))
         await message.channel.send(f"``` {source}```    ")
+    
+    else:
+        term = search_term(message.content.lower())
+        if term in key:
+            render = parse_single(links[term])
+            if render:
+                no = len(render)
+                actual_no = no/2
+                await message.channel.send(f"{actual_no} news stories incoming")
+                for y in render:
+                    await message.channel.send(y)
+                    print(y)
+            else:
+                await message.channel.send(f"Unable to fetch news from {term}")
+                print("Unable to fetch news from {term}")
+        else:
+            print("Error in Term finding")
+            await message.channel.send(f"Error in finding term from {message.content}")
+
 
 client.run(bot_token, reconnect=True)
